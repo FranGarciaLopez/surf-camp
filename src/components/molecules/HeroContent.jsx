1e3ccd89f2fd9a7cf3ce31b0f23ece131caa466b
FranGarciaLopez <frangalo34@gmail.com>
2025-07-03 14:57:49 +0000

    update hero image

--- src/components/molecules/HeroContent.jsx
+++ src/components/molecules/HeroContent.jsx
@@ -3,28 +3,26 @@ import Title from "../atoms/Title";
 import Button from "../atoms/Button";
 
 const HeroContent = () => (
-  <section className="flex flex-col h-screen overflow-hidden">
-    <div className="relative h-screen">
-      <Image
-        src="https://res.cloudinary.com/dgvvnopu7/image/upload/q_auto,f_auto/v1751452411/hero-img_kjdfig.avif"
-        alt="Hero Image"
-        className="w-full h-full object-cover"
-      />
-      <div className="absolute inset-0 bg-black opacity-20"></div>
+  <section className="relative h-screen overflow-hidden">
+    <Image
+      src="https://res.cloudinary.com/dgvvnopu7/image/upload/q_auto,f_auto/v1751452411/hero-img_kjdfig.avif"
+      alt="Hero Image"
+      className="w-full h-full object-cover scale-110"
+    />
+    <div className="absolute inset-0 bg-black opacity-20"></div>
 
-      <div className="absolute inset-0 flex flex-col items-center justify-end my-40 mx-20 lg:items-start lg:justify-center">
-        <Title>
-          <Image
-            src="https://res.cloudinary.com/dgvvnopu7/image/upload/v1751452476/text_gzq7ki.svg"
-            alt="Logo"
-            draggable="false"
-            className="w-80 mb-4 lg:w-96 lg:mb-6 xl:w-110 xl:mb-8"
-          />
-        </Title>
-        <Button label="Join now the experience" />
-      </div>
+    <div className="absolute inset-0 flex flex-col items-center justify-end my-40 mx-20 lg:items-start lg:justify-center">
+      <Title>
+        <Image
+          src="https://res.cloudinary.com/dgvvnopu7/image/upload/v1751452476/text_gzq7ki.svg"
+          alt="Logo"
+          draggable="false"
+          className="w-80 mb-4 lg:w-96 lg:mb-6 xl:w-110 xl:mb-8"
+        />
+      </Title>
+      <Button label="Join now the experience" />
     </div>
-  </section>
+  </section >
 );
 
 export default HeroContent;
